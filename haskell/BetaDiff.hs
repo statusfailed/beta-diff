79be370ebbf21438cd36f66a666874c3533dd7ba
status failed <sf@statusfailed.com>
2015-05-18 20:19:39 +0000

    tidied haskell a little, renamed pBetaDiff to betaDiff

--- haskell/BetaDiff.hs
+++ haskell/BetaDiff.hs
@@ -1,4 +1,4 @@
-module Beta (pBetaDiff) where
+module Beta (betaDiff) where
 
 import Numeric.SpecFunctions (logBeta)
 
@@ -11,16 +11,22 @@ h a1 b1 a2 b2 = exp $ logBeta (fromIntegral $ a1 + a2) (fromIntegral $ b1 + b2) 
         a2' = fromIntegral a2
         b2' = fromIntegral b2
 
--- Recursive scheme from paper
 g :: Int -> Int -> Int -> Int -> Double
 g a1 b1 a2 b2
   | a1 == a2 && b1 == b2 = 0.5 
-  | a1 >  a2             = g (a1 - 1) b1 a2 b2 + (h (a1 - 1) b1 a2 b2 / fromIntegral (a1 - 1)) 
-  | a1 <  a2             = g a1 b1 (a2 - 1) b2 - (h a1 b1 (a2 - 1) b2 / fromIntegral (a2 - 1)) 
-  |             b1 >  b2 = g a1 (b1 - 1) a2 b2 - (h a1 (b1 - 1) a2 b2 / fromIntegral (b1 - 1)) 
-  |             b1 <  b2 = g a1 b1 a2 (b2 - 1) + (h a1 b1 a2 (b2 - 1) / fromIntegral (b2 - 1)) 
+  | a1 >  a2             = f (+) (a1 - 1) b1 a2 b2 (a1 - 1)
+  | a1 <  a2             = f (-) a1 b1 (a2 - 1) b2 (a2 - 1)
+  |             b1 >  b2 = f (-) a1 (b1 - 1) a2 b2 (b1 - 1)
+  |             b1 <  b2 = f (+) a1 b1 a2 (b2 - 1) (b2 - 1)
   | otherwise            = -1
+  where
+    -- This expression is shared among each 4 cases with different arguments,
+    -- so I broke it out into this function "f".
+    -- First arg: plus or minus function to use
+    -- last argument: denominator
+    f (+/-) a1 b1 a2 b2 d = g a1 b1 a2 b2 +/-
+                              (h a1 b1 a2 b2 / fromIntegral d)
 
 
-pBetaDiff :: Int -> Int -> Int -> Int -> Double
-pBetaDiff = g
+betaDiff :: Int -> Int -> Int -> Int -> Double
+betaDiff = g
